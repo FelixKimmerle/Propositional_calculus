a797c7bf88fbf8d90ab97f44d8a5bd607a09a2f4
Felix Kimmerle <Felixkimmerle@hotmail.com>
2018-03-31 00:51:31 +0200

    first commit

--- Lexer.h
+++ Lexer.h
@@ -2,56 +2,13 @@
 #include "Token.h"
 #include <algorithm>
 #include <map>
+#include <vector>
 namespace Lexer
 {
-struct LexerResult
-{
-	std::vector<Token> Tokens;
-	std::vector<char> Variables;
-};
-std::map<char, Token::Types> m_Definitions = {
-	{'&', Token::Types::AND},
-	{'|', Token::Types::OR},
-	{'=', Token::Types::EQUAL},
-	{'!', Token::Types::NOT},
-	{'(', Token::Types::BOPEN},
-	{')', Token::Types::BCLOSE},
-	{'-', Token::Types::IMPLICATION},
-	{'^', Token::Types::XOR},
-};
-
-const std::string m_Ignore = ">";
-
-LexerResult Tokenize(std::string text)
-{
-	LexerResult LResult;
-	for (int i = 0; i < text.size(); i++)
+	struct LexerResult
 	{
-		if (m_Ignore.find(text[i]) != std::string::npos)
-		{
-			continue;
-		}
-		if (m_Definitions.count(text[i]))
-		{
-			LResult.Tokens.push_back(Token(m_Definitions[text[i]], text[i], i));
-		}
-		else
-		{
-			if (text[i] == '0' || text[i] == '1')
-			{
-				LResult.Tokens.push_back(Token(Token::Types::NUM, text[i], i));
-			}
-			else
-			{
-				LResult.Tokens.push_back(Token(Token::Types::VAR, text[i], i));
-				auto it = std::find(LResult.Variables.begin(), LResult.Variables.end(), text[i]);
-				if (it == LResult.Variables.end())
-				{
-					LResult.Variables.push_back(text[i]);
-				}
-			}
-		}
-	}
-	return LResult;
-}
+		std::vector<Token> Tokens;
+		std::vector<char> Variables;
+	};
+	LexerResult Tokenize(std::string text);
 };
